7aedc7acfa3724dad5a4da2b0b021b20af4d9fe0
NoTabaco <adslonese@gmail.com>
2023-01-15 22:30:11 +0900

    Fetching Data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import { AppProps } from "next/dist/shared/lib/router/router";
+import { QueryClient, QueryClientProvider } from "react-query";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </Layout>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,47 @@
+import { useQuery } from "react-query";
 import Seo from "../components/Seo";
 
+const API_KEY = "fbd978aee26f62ff878703caa3fed923";
+
+interface IGetPopularMoviesProps {
+  page: number;
+  results: IMovieProps[];
+  total_pages: number;
+  total_results: number;
+}
+
+interface IMovieProps {
+  id: number;
+  original_title: string;
+}
+
+const fetchPopularMovies = async () => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+  );
+  return res.json();
+};
+
 export default function Home() {
+  const { data, isLoading } = useQuery<IGetPopularMoviesProps>(
+    "popularMovies",
+    fetchPopularMovies
+  );
+
   return (
     <div>
       <Seo title="Home" />
-      <h1>Hello</h1>
+      {isLoading ? (
+        <h4>Loading...</h4>
+      ) : (
+        data?.results.map(movie => {
+          return (
+            <div key={movie.id}>
+              <h4>{movie.original_title}</h4>
+            </div>
+          );
+        })
+      )}
     </div>
   );
 }
