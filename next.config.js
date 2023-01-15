a15b4630869ed74ef76557839beeb36dc5450ea3
NoTabaco <adslonese@gmail.com>
2023-01-16 01:15:57 +0900

    Redirects and Rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
+const API_KEY = process.env.API_KEY;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import { useQuery } from "react-query";
 import Seo from "../components/Seo";
 
-const API_KEY = "fbd978aee26f62ff878703caa3fed923";
-
 interface IGetPopularMoviesProps {
   page: number;
   results: IMovieProps[];
@@ -13,12 +11,11 @@ interface IGetPopularMoviesProps {
 interface IMovieProps {
   id: number;
   original_title: string;
+  poster_path: string;
 }
 
 const fetchPopularMovies = async () => {
-  const res = await fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-  );
+  const res = await fetch(`/api/movies`);
   return res.json();
 };
 
@@ -29,19 +26,39 @@ export default function Home() {
   );
 
   return (
-    <div>
+    <div className="container">
       <Seo title="Home" />
       {isLoading ? (
         <h4>Loading...</h4>
       ) : (
-        data?.results.map(movie => {
-          return (
-            <div key={movie.id}>
-              <h4>{movie.original_title}</h4>
-            </div>
-          );
-        })
+        data?.results.map(movie => (
+          <div key={movie.id} className="movie">
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+            <h4>{movie.original_title}</h4>
+          </div>
+        ))
       )}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   );
 }
