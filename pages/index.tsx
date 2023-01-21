3a2a6451d37d059478f012812127b43497f76a62
NoTabaco <adslonese@gmail.com>
2023-01-22 00:16:47 +0900

    Server Side Rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,5 @@
-import { useQuery } from "react-query";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import Seo from "../components/Seo";
-
-interface IGetPopularMoviesProps {
-  page: number;
-  results: IMovieProps[];
-  total_pages: number;
-  total_results: number;
-}
 
 interface IMovieProps {
   id: number;
@@ -14,30 +7,18 @@ interface IMovieProps {
   poster_path: string;
 }
 
-const fetchPopularMovies = async () => {
-  const res = await fetch(`/api/movies`);
-  return res.json();
-};
-
-export default function Home() {
-  const { data, isLoading } = useQuery<IGetPopularMoviesProps>(
-    "popularMovies",
-    fetchPopularMovies
-  );
-
+export default function Home({
+  results,
+}: InferGetServerSidePropsType<GetServerSideProps>) {
   return (
     <div className="container">
       <Seo title="Home" />
-      {isLoading ? (
-        <h4>Loading...</h4>
-      ) : (
-        data?.results.map(movie => (
-          <div key={movie.id} className="movie">
-            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-            <h4>{movie.original_title}</h4>
-          </div>
-        ))
-      )}
+      {results?.map((movie: IMovieProps) => (
+        <div key={movie.id} className="movie">
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
       <style jsx>{`
         .container {
           display: grid;
@@ -61,4 +42,16 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch("http://localhost:3000/api/movies")
+  ).json();
+
+  return {
+    props: {
+      results,
+    },
+  };
 }
